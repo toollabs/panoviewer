af478184498c1cb1f9c1b3a0551ef2297022ee0a
Daniel Schwen <daniel@schwen.de>
2016-03-22 22:11:04 +0000

    Fix typos

--- public_html/cache.php
+++ public_html/cache.php
@@ -33,7 +33,7 @@ if (mysqli_num_rows($res) == 1)
 }
 else
 {
-  echo "Database error."
+  echo "Database error.";
   exit;
 }
 
@@ -67,7 +67,7 @@ if (!is_readable($c))
   if ($t == '')
   {
     $image = file_get_contents($fullfile, false, null, 0, 100);
-    if (substr($image,6,4) != 'JFIF' && substr($image,6,4) != 'Exif' && substr($image,6,9) != 'Photoshop'))
+    if (substr($image,6,4) != 'JFIF' && substr($image,6,4) != 'Exif' && substr($image,6,9) != 'Photoshop')
       exit;
   }
 

60b35c585df401884498f5133dd997b385ea39d2
Daniel Schwen <daniel.schwen@inl.gov>
2016-06-22 14:21:32 +0200

    add background, add tiered json output

--- public_html/config.php
+++ public_html/config.php
@@ -1,18 +1,25 @@
 <?php
 // all images above this size are tiled (and temporarily shown as a max_size rescaled version)
-$max_size = 4000;
+$max_width = 4000;
+
+// send content type header and prevent caching
+header('Content-Type: application/json');
+header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: 0");
 
 // get normalized filename
 $f = str_replace(' ', '_', ucfirst($_GET['f']));
 if ($f == "")
 {
-  echo "Supply a filename!";
+  echo '{ "error": "No file name supplied" }';
   exit;
 }
 
 // cache identifier
 $md5 = md5($f);
-$c = 'cache/' . $md5 . '.jpg';
+$cache_prefix = 'cache/' . $md5;
+$cache_file = $cache_prefix . '.jpg';
 
 // connect to database
 $ts_pw = posix_getpwuid(posix_getuid());
@@ -26,7 +33,7 @@ $res = mysqli_query($db, $sql);
 
 if (mysqli_num_rows($res) != 1)
 {
-  echo "Database error.";
+  echo '{ "error": "Database error (found ' . mysqli_num_rows($res) . 'results; should be 1)" }';
   exit;
 }
 
@@ -34,16 +41,12 @@ if (mysqli_num_rows($res) != 1)
 $row = mysqli_fetch_array($res);
 $width = intval($row['img_width']);
 
-header('Content-Type: application/json');
-echo '{ "popel": "nase" }';
-exit;
-
 // see if we have an up to date untiled version in the cache
 $fetch_file = false;
-if (is_readable($c))
+if (is_readable($cache_file))
 {
   // get cache modification time
-  $ctime = strftime("%Y%m%d%H%M%S", filectime($c));
+  $ctime = strftime("%Y%m%d%H%M%S", filectime($cache_file));
 
   // we need to re-fetch the file if the last upload reported by the database is NEWER than the file we have cached
   $fetch_file =  $row['img_timestamp'] > $ctime;
@@ -51,36 +54,71 @@ if (is_readable($c))
 else
   $fetch_file = true;
 
-if (!is_readable($c))
+// need to (re-)fetch the file from commons
+if ($fetch_file)
 {
-  if ($t == '')
-    $fullfile = 'https://upload.wikimedia.org/wikipedia/commons/' . substr($md5,0,1) . '/' . substr($md5,0,2) . '/' . $f;
-  else
-    $fullfile = 'https://commons.wikimedia.org/w/thumb.php?w=' . $t . '&f=' . $f;
-
   // either not cached before, or cached version too old
   ini_set('user_agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.9) Gecko/20071025 Firefox/2.0.0.9');
 
-  // if we are not getting a thumbnail make sure the file is a valid JPG file
-  if ($t == '')
+  // make sure the file is a valid JPG file
+  $fullfile = 'https://upload.wikimedia.org/wikipedia/commons/' . substr($md5, 0, 1) . '/' . substr($md5, 0, 2) . '/' . $f;
+  $image = file_get_contents($fullfile, false, null, 0, 100);
+  if (substr($image, 6, 4) != 'JFIF' &&
+      substr($image, 6, 4) != 'Exif' &&
+      substr($image, 6, 9) != 'Photoshop')
   {
-    $image = file_get_contents($fullfile, false, null, 0, 100);
-    if (substr($image,6,4) != 'JFIF' && substr($image,6,4) != 'Exif' && substr($image,6,9) != 'Photoshop')
-      exit;
+    echo '{ "error": "This file does not look like a valid JPEG" }';
+    exit;
   }
 
+  // for large images we prepare a downscaled preview while the tiling is in progress
+  if ($width > $max_width)
+  {
+    $preview = 'https://commons.wikimedia.org/w/thumb.php?w=' . $max_width . '&f=' . $f;
+
+    // TODO dispatch the tiling job in the background, while we serve the preview
+  }
+  else
+    $preview = $fullfile;
+
+  // open the file on commons and the local cache version
   umask(022);
-  $handle = fopen($c, 'wb');
+  $src = fopen($preview, 'rb');
+  $dst = fopen($cache_file, 'wb');
+  if (!$src || !$dst) exit;
 
-  $handle2 = fopen($fullfile, 'rb');
-  if (!$handle2) exit;
-
-  while (!feof($handle2))
+  // copy the file in 8kB chunks
+  while (!feof($src))
   {
-    $chunk = fread($handle2, 8192);
-    fwrite($handle, $chunk);
+    $chunk = fread($src, 8192);
+    fwrite($dst, $chunk);
   }
-  fclose($handle2);
-  fclose($handle);
+  fclose($src);
+  fclose($dst);
 }
+
+// build the return object
+$cache_config = $cache_prefix . '/config.json';
+if (is_readable($cache_config))
+{
+  // redirect to config.json file
+  $config = json_decode(file_get_contents($cache_config));
+  $config->multiRes->path = $cache_prefix . $json->multiRes->path;
+  $config->multiRes->fallbackPath = $dir . $json->multiRes->fallbackPath;
+  $json = (object) [
+    'pannellum' => $config
+  ];
+}
+else
+{
+  // return the preview (which is the full version for small images)
+  $json = (object) [
+    'multires_pending' => ($width > $max_width),
+    'pannellum' => (object)[
+      'panorama' => $cache_file
+    ]
+  ];
+}
+
+echo json_encode($json);
 ?>
